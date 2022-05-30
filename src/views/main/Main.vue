7b4ccb2e97006b3cf20406ce34b53567dad9d66d
andersonar12 <aromerocangri.14@gmail.com>
2022-05-30 15:22:07 -0400

    Fetch with timeout "AbortController"

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -15,6 +15,28 @@ export default {
   components: {
     Navbar,
   },
+  mounted(){
+
+  
+  //Fetch with timeout "AbortController"
+
+  const timeout = () =>{
+    const controller = new AbortController();
+    setTimeout(() => controller.abort(), 10000);
+    return controller
+  }
+
+    let requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+      signal: timeout().signal  
+    };
+
+    fetch("http://localhost:3000/test", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('Fetch timeout request', error))
+  }
 };
 </script>
 
